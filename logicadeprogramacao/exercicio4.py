77cf08fd4f197883937f640e4ca484b18062baff
mtpachecoo <matheusalmeida13@hotmail.com>
2025-03-18 23:18:38 -0300

    segundo commit

---
+++ logicadeprogramacao/exercicio4.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Digite o prmeiro numero:'))
+numero2 = int(input('Digite o segundo numero:'))
+
+if numero1 < numero2:
+    print(f'Ordem crescente: {numero1}, {numero2}')
+else:
+    print(f'Ordem crescente:{numero2}, {numero1}')
